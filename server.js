ab5db66dddba7935da773066cfd640fc961470ae
Hackerboi404 <tuturahul14rk@gmail.com>
2026-05-03 11:59:08 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const axios = require('axios');
 const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http, {
@@ -7,51 +6,6 @@ const io = require('socket.io')(http, {
 });
 
 app.use(express.static('public'));
-
-// --- GOOGLE DRIVE & TERABOX STREAMING PROXY ---
-// Ye route video ko server ke through pass karega taaki CORS error na aaye
-app.get('/stream', async (req, res) => {
-    const videoUrl = req.query.url;
-    
-    if (!videoUrl) {
-        return res.status(400).send('URL parameter is required');
-    }
-
-    try {
-        // Google Drive link ko convert karna
-        let finalUrl = videoUrl;
-        
-        if (videoUrl.includes('drive.google.com')) {
-            // ID nikalna
-            const idMatch = videoUrl.match(/\/d\/(.*?)\//);
-            if (idMatch && idMatch[1]) {
-                const id = idMatch[1];
-                finalUrl = `https://drive.google.com/uc?export=download&id=${id}`;
-            }
-        } else if (videoUrl.includes('terabox')) {
-            // TeraBox direct link kaam nahi karta server side easily, 
-            // isliye wo error dega.
-            return res.status(400).send("TeraBox is not supported via Proxy. Please use Google Drive.");
-        }
-
-        console.log(`Proxying: ${finalUrl}`);
-
-        // Video ko fetch karna
-        const response = await axios({
-            method: 'get',
-            url: finalUrl,
-            responseType: 'stream'
-        });
-
-        // Headers set karna taaki browser ko lage video file hai
-        res.setHeader('Content-Type', 'video/mp4');
-        response.data.pipe(res);
-
-    } catch (error) {
-        console.error('Stream Error:', error.message);
-        res.status(500).send('Error loading video');
-    }
-});
 
 // --- STATE VARIABLES ---
 let currentVideo = null;
